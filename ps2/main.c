f57087540da69f2fab657ade8d40d3c3620df829
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-13 23:44:23 +0100

    Update file main.c

--- ps2/main.c
+++ ps2/main.c
@@ -18,4 +18,5 @@ struct game game = {
 printf("is won: %d\n", is_game_won(game));
 printf("is move possible: %d\n", is_move_possible(game));
 bool result = update(&game, 0, -1);
+printf("Result: %s\n", result ? "true" : "false");
 }
