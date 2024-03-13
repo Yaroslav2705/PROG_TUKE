1a6826a92a81457f68424a5f1097a81cb8bda48f
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-13 18:21:17 +0100

    Update file k.c

--- ps2/k.c
+++ ps2/k.c
@@ -3,41 +3,44 @@
 #include <stdbool.h>
 #include "k.h"
 
-struct game {
-        char board[4][4];
-        char score;
-};
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "k.h"
 
-bool is_game_won(const struct game game){
-        for(int kak = 0; kak < 4; kak++){
-                for(int kok = 0; kok < 4; kok++){
-                        if(game.board[kak][kok] == 'K')
-                                return true;
-                }
+// Реализации ваших функций add_random_tile, update и т. д.
+
+bool is_game_won(const struct game game) {
+    for (int kak = 0; kak < SIZE; kak++) {
+        for (int kok = 0; kok < SIZE; kok++) {
+            if (game.board[kak][kok] == 'K')
+                return true;
         }
-        return false;
+    }
+    return false;
 }
 
-bool is_move_possible(const struct game game){
-        for(int kak = 0; kak < 3; kak++){
-                for(int kok = 0; kok < 3; kok++){
-                        if(game.board[kak][kok] == ' ')
-                                return true;
+bool is_move_possible(const struct game game) {
+    for (int kak = 0; kak < SIZE - 1; kak++) {
+        for (int kok = 0; kok < SIZE - 1; kok++) {
+            if (game.board[kak][kok] == ' ')
+                return true;
 
-                        if(game.board[kak][kok] == game.board[kak+1][kok] || game.board[kak][kok] == game.board[kak][kok+1])
-                                return true;
-                }
+            if (game.board[kak][kok] == game.board[kak + 1][kok] || game.board[kak][kok] == game.board[kak][kok + 1])
+                return true;
         }
+    }
 
-        for(int kik = 0; kik < 3; kik++){
-                if(game.board[3][kik] == ' ' || game.board[kik][3] == ' ')
-                        return true;
+    for (int kik = 0; kik < SIZE - 1; kik++) {
+        if (game.board[SIZE - 1][kik] == ' ' || game.board[kik][SIZE - 1] == ' ')
+            return true;
 
-                if(game.board[3][kik] == game.board[3][kik+1] || game.board[kik][3] == game.board[kik+1][3])
-                        return true;
-        }
-        return false;
+        if (game.board[SIZE - 1][kik] == game.board[SIZE - 1][kik + 1] || game.board[kik][SIZE - 1] == game.board[kik + 1][SIZE - 1])
+            return true;
+    }
+    return false;
 }
+
 
 /*
 void add_random_tile(struct game *game){
