01875f2b547154f95b7911ba6cfc6a54e814a8b3
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-15 14:59:00 +0100

    Update 2 files
    - /ps2/hof.c
    - /ps2/main.c

--- ps2/main.c
+++ ps2/main.c
@@ -19,4 +19,15 @@ printf("is won: %d\n", is_game_won(game));
 printf("is move possible: %d\n", is_move_possible(game));
 bool result = update(&game, 0, -1);
 printf("Result: %s\n", result ? "true" : "false");
-}
+
+int num_loaded = load(list);
+
+    if (num_loaded == -1) {
+        printf("Ошибка при загрузке файла.\n");
+        return 1;
+    }
+
+    printf("Загружено %d игроков из файла:\n", num_loaded);
+    for (int i = 0; i < num_loaded; i++) {
+        printf("%s - %d\n", list[i].name, list[i].score);
+    }}
