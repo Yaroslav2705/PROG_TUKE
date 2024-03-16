1ced4d8ee0bd997d934e7b13a9f11d2641d2de5b
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-16 10:17:14 +0100

    Update file hof.c

--- ps2/hof.c
+++ ps2/hof.c
@@ -57,7 +57,7 @@ int load(struct player list[]) {
     FILE *file = fopen("score.txt", "r");
     if (file == NULL) {
         //printf("Error opening file.\n");
-        return -1; // Return -1 to indicate error
+        return 0; // Return -1 to indicate error
     }
 
     int count = 0;
