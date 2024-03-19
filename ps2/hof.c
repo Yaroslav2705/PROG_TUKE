30e3b1e50fc9d259b421daf4b72ba565f588bff0
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-19 17:55:29 +0100

    Update file hof.c

--- ps2/hof.c
+++ ps2/hof.c
@@ -48,45 +48,6 @@ bool save(const struct player list[], const int size) {
     return true; // Return true to indicate success
 }
 
-/*
-
-bool add_player(struct player list[], int *size, const struct player new_player) {
-    // Find the insertion index
-    int insert_index = *size;
-    for (int i = 0; i < *size; i++) {
-        if (new_player.score >= list[i].score) {
-            insert_index = i;
-            break;
-        }
-    }
-
-    // Shift players to the right to make room for the new player
-    for (int i = *size; i > insert_index; i--) {
-        list[i] = list[i - 1];
-    }
-
-    // Copy the data of the new player to the correct position in the list
-    list[insert_index] = new_player;
-
-    // Increase the size of the list
-    (*size)++;
-
-    // If the size exceeds 10, remove the last player
-    if (*size > 10) {
-        (*size)--;
-    }
-
-    // Save the changes to the file
-    bool saved = save(list, *size);
-
-    if (!saved && *size == 10 && new_player.score > list[9].score) {
-    return false;
-}
-
-
-    return true;
-}*/
-
 bool add_player(struct player list[], int *size, const struct player new_player) {
     // Find the insertion index
     int insert_index = *size;
