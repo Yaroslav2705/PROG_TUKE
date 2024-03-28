ef68c4d954ce43c934009f2c0556bcb2b275a35e
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-28 16:23:10 +0100

    Update file mm.c

--- ps3/mm.c
+++ ps3/mm.c
@@ -2,45 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sum_of_digits(char *olynnik) {
+// Function to calculate the sum of digits of a number represented as a string
+int mas_of_yura(char *olinnyk) {
     int mas = 0;
-    for (int i = 0; olynnik[i] != '\0'; i++) {
-        if (olynnik[i] >= '0' && olynnik[i] <= '9') { // Check if the character is a digit
-            mas += olynnik[i] - '0';
-        } else {
-            // If the character is not a digit, print an error message or handle it differently
-            //printf("Error: Invalid data\n");
-            return -1; // Return a negative value to indicate an error
-        }
+    for (int i = 0; olinnyk[i] != '\0'; i++) {
+        mas += olinnyk[i] - '0';
     }
-    
     return mas;
 }
 
-
 // Function to apply the MMM (Mighty Method of Mathematics)
-int MMM(char *olynnik) {
-    int result = sum_of_digits(olynnik);
+int MMM(char *olinnyk) {
+    int result = mas_of_yura(olinnyk);
     while (result >= 10) {
-        char str_result[4]; // space for maximum of 3 digits and null terminator
-        snprintf(str_result, sizeof(str_result), "%d", result);
-        result = sum_of_digits(str_result);
+        char str_result[20]; // a sufficiently large buffer to represent the result
+        snprintf(str_result, 20, "%d", result);
+        result = mas_of_yura(str_result);
     }
     return result;
 }
 
 int main() {
-    char olynnik[1000]; // assuming the number can be up to 999 digits long
-	int yura;
+    char olinnyk[100000]; // assuming the number can be up to 999 digits long
+    scanf("%s", olinnyk); // Input the number
 
-	scanf("%d", &yura);	
-	while(yura < 1){
-    scanf("%d", &yura); // Input the number
-	}
-	
-    sprintf(olynnik, "%d", yura);
-
-    int result = MMM(olynnik); // Apply the MMM
+    int result = MMM(olinnyk); // Apply the MMM
 
     printf("%d\n", result); // Output the result
 
