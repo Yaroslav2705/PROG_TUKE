cff447212c09e98b35bc6086a75d9bfe70ab016b
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-03-31 17:53:34 +0200

    Update file racer.c

--- ps3/racer.c
+++ ps3/racer.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <math.h>
 
-int n;
-double t;
-double s[1005], v[1005];
-
-double total_time(double k, double max_speed, double min_speed) {
-    double total = 0;
+double calculate_time(double *distances, double *velocities, int n, double k) {
+    double total_time = 0;
     for (int i = 0; i < n; ++i) {
-        double actual_speed = v[i] + k;
-        total += s[i] / (fabs(actual_speed) > fabs(min_speed) ? fabs(actual_speed) : fabs(min_speed));
+        double actual_velocity = velocities[i] + k;
+        total_time += distances[i] / actual_velocity;
     }
-    return total;
+    return total_time;
 }
 
-double binary_search(double low, double high, double max_speed, double min_speed) {
-    double epsilon = 1e-7;
-    while (high - low > epsilon) {
-        double mid = (low + high) / 2.0;
-        if (total_time(mid, max_speed, min_speed) > t)
-            high = mid;
-        else
-            low = mid;
+double find_constant(double *distances, double *velocities, int n, double t) {
+    double left = -1000, right = 1000;
+    double precision = 1e-7;
+
+    while (right - left > precision) {
+        double mid = (left + right) / 2;
+        double time = calculate_time(distances, velocities, n, mid);
+        if (time <= t) {
+            right = mid;
+        } else {
+            left = mid;
+        }
     }
-    return low;
+
+    if (calculate_time(distances, velocities, n, right) <= t)
+        return right;
+    return left;
 }
 
 int main() {
+    int n;
+    double t;
     scanf("%d %lf", &n, &t);
-    double max_speed = 0, min_speed = 0;
+
+    double distances[n], velocities[n];
     for (int i = 0; i < n; ++i) {
-        scanf("%lf %lf", &s[i], &v[i]);
-        max_speed = fmax(max_speed, v[i]);
-        min_speed = fmin(min_speed, v[i]);
+        scanf("%lf %lf", &distances[i], &velocities[i]);
     }
 
-    double result = binary_search(-max_speed, -min_speed, max_speed, min_speed);
-    printf("%.7lf\n", result);
+    double result = find_constant(distances, velocities, n, t);
+    printf("%.9lf\n", result);
 
     return 0;
 }
