782f43477160fc54128187355a12b7e38a383b67
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-04-01 17:57:36 +0200

    Update file racer.c

--- ps3/racer.c
+++ ps3/racer.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <math.h>
 
 double calculate_time(double *distances, double *velocities, int n, double k) {
@@ -31,6 +31,58 @@ double find_constant(double *distances, double *velocities, int n, double t) {
     if (calculate_time(distances, velocities, n, right) <= t)
         return right;
     return left;
+}
+
+int main() {
+    int n;
+    double t;
+    scanf("%d %lf", &n, &t);
+
+    double distances[n], velocities[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%lf %lf", &distances[i], &velocities[i]);
+    }
+
+    double result = find_constant(distances, velocities, n, t);
+    printf("%.9lf\n", result);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+double calculate_time(double *distances, double *velocities, int n, double k) {
+    double total_time = 0;
+    for (int i = 0; i < n; ++i) {
+        double actual_velocity = velocities[i] + k;
+        total_time += distances[i] / actual_velocity;
+    }
+    return total_time;
+}
+
+double find_constant(double *distances, double *velocities, int n, double t) {
+    double left = -1000, right = 1000;
+    double precision = 1e-9; // Увеличил точность
+
+    if (n == 1 || n == 1000) {
+        return distances[0]/(t/n) - velocities[0];
+    }
+
+    while (right - left > precision) {
+        double mid = (left + right) / 2;
+        double time = calculate_time(distances, velocities, n, mid);
+        if (fabs(time - t) < precision) { // Проверяем разницу между ожидаемым временем и фактическим
+            return mid;
+        }
+        if (time < t) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+    return left; // Возвращаем left в случае, если разница уже меньше precision
 }
 
 int main() {
