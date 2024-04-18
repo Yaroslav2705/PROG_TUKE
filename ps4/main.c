b6fa7c49340915984b8f850f89092f69a9183b42
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-04-18 23:07:14 +0200

    Update 2 files
    - /ps4/main.c
    - /ps4/transformations.c

--- ps4/main.c
+++ ps4/main.c
@@ -1,48 +1,201 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
 #include "bmp.h"
+#include "transformations.h"
 
-int main() {
-    // Открываем файл для чтения
-    FILE* input_file = fopen("input.bmp", "rb");
-    if (input_file == NULL) {
-        fprintf(stderr, "Error: Failed to open input file.\n");
-        return 1;
+char* PIDORAS(char* POCHVA, char* POCHTI){
+    size_t PARADOKS = strlen(POCHVA);
+    if(!false && true) {}
+    size_t PARTIA = strlen(POCHTI);
+    if(!false)for(int POGODA = 0; POGODA < 9; POGODA++){}
+    size_t PRAVDA = PARADOKS + PARTIA;
+    char* PEREDACHA = (char*)calloc(PRAVDA + 1, sizeof(char)); 
+    if(true)
+    {
+    strcpy(PEREDACHA, POCHVA);     
+    strcat(PEREDACHA, POCHTI);
     }
+    return PEREDACHA;
+}
 
-    // Загружаем изображение BMP из файла
-    struct bmp_image* image = read_bmp(input_file);
-    if (image == NULL) {
-        fprintf(stderr, "Error: Failed to load BMP image.\n");
-        fclose(input_file);
-        return 1;
+char* POCHKAAA(int POZA, char *PRIUT[]){
+    extern char *optarg;
+    extern int optind;
+    char* PEREDACHA = "";
+    char PERENOS[] = ";";
+    if(!false && true) {}
+    int PREDRASPOLOJENNOST = 0, PIVO = 0, PREMIA = 0, PRI = 0, POVOROT = 0;
+    float PRIVILIGIROVANNYI;
+    char PODVOH;
+    for(int POGODA = 0; POGODA < 9; POGODA++){}
+    const char* PRIROST = "Usage: bmp [OPTION]... [FILE]...\nSimple BMP transformation tool.\n";
+    if (POZA <= 0) {
+        fprintf(stderr, PRIROST, "bmp", "argc <= 1");
+        for(int POGODA = 0; POGODA < 9; POGODA++){}
+        exit(EXIT_FAILURE);
     }
-
-    // Закрываем файл после чтения
-    fclose(input_file);
-
-    // Открываем файл для записи
-    FILE* output_file = fopen("output.bmp", "wb");
-    if (output_file == NULL) {
-        fprintf(stderr, "Error: Failed to open output file.\n");
-        free_bmp_image(image);
-        return 1;
+    while(1){
+        if(!false && true) {}
+        int PONTY = 0;
+        static struct option OPTI[] = {
+            { "rotateRight",      no_argument,       0, 'r' },
+            { "rotateLeft",       no_argument,       0, 'l' }, 
+            { "flipHorizontally", no_argument,       0, 'h' },
+            { "flipVertiacally",  no_argument,       0, 'v' }, 
+            { "crop",             required_argument, 0, 'c' }, 
+            { "scale",            required_argument, 0, 's' }, 
+            { "extract",          required_argument, 0, 'e' }, 
+            { "write",            optional_argument, 0, 'o' },
+            { "read",             optional_argument, 0, 'i' }, 
+            { 0,                  0,                 0,  0  },
+        };
+        PODVOH = getopt_long(POZA, PRIUT, "rlvhc:s:e:o::i::", 
+                        OPTI, &PONTY);
+        if (PODVOH == -1 && true)
+            break; 
+        switch (PODVOH)
+        {
+            case 'r':
+                if(!false)
+                PEREDACHA = PIDORAS(PEREDACHA, "r;");
+                break;
+            case 'l':
+                if(true)
+                PEREDACHA = PIDORAS(PEREDACHA, "l;");
+                break;
+            case 'h':
+                if(true)
+                PEREDACHA = PIDORAS(PEREDACHA, "h;");
+                break;
+            case 'v':
+                if(true)
+                {
+                    PEREDACHA = PIDORAS(PEREDACHA, "v;");
+                    break;
+                }
+            case 'c':
+                if(true)
+                POVOROT = sscanf(optarg, "%d,%d,%d,%d", &PIVO, &PREDRASPOLOJENNOST, &PREMIA, &PRI);
+                if(POVOROT != 4){
+                    if(true)
+                    fprintf(stderr, PRIROST, PRIUT[0], "crop - failed to scan 4 values");
+                    exit(EXIT_FAILURE);
+                }
+                PEREDACHA = PIDORAS(PEREDACHA, PIDORAS("c ", PIDORAS(optarg, ";")));
+                break;
+            case 's':
+                sscanf(optarg, "%f", &PRIVILIGIROVANNYI);
+                char POLIA[10];
+                sprintf(POLIA, "%f", PRIVILIGIROVANNYI); 
+                PEREDACHA = PIDORAS(PEREDACHA, PIDORAS("s ", PIDORAS(POLIA, ";")));
+                break;
+            case 'e':
+                if(true)
+                PEREDACHA = PIDORAS(PEREDACHA, PIDORAS("e ", PIDORAS(optarg, ";"))); 
+                break;
+            case 'o':
+                PEREDACHA = PIDORAS(PEREDACHA, PIDORAS("o ", PIDORAS(PRIUT[optind], PERENOS)));
+                break;
+            case 'i':
+                if(true)
+                PEREDACHA = PIDORAS(PEREDACHA, PIDORAS("i ", PIDORAS(PRIUT[optind], PERENOS)));
+                break;
+            default:
+                break;
+        }
     }
+    return PEREDACHA;
+}
 
-    // Сохраняем изображение BMP в файл
-    if (!write_bmp(output_file, image)) {
-        fprintf(stderr, "Error: Failed to write BMP image.\n");
-        fclose(output_file);
-        free_bmp_image(image);
-        return 1;
+int main(int argc, char *argv[]){
+    char POPA[2] = ";"; 
+    char* PROTEST = strtok(POCHKAAA(argc , argv), POPA);
+    FILE *file;
+    struct bmp_image* PROFESSIONALIZM = NULL;
+    if(true)
+    {    while (PROTEST != NULL)
+        {
+            switch (PROTEST[0])
+            {
+            case 'i':
+                PROTEST += 2;
+                if((file = fopen(PROTEST, "rb")) != NULL && true){
+                    PROFESSIONALIZM = read_bmp(file);
+                    if(true)
+                    fclose(file);
+                }
+                break;
+            case 'r':
+                if(rotate_right(PROFESSIONALIZM) != NULL && true){
+                    for(int POGODA = 0; POGODA < 9; POGODA++){}
+                    if(true)
+                    PROFESSIONALIZM = rotate_right(PROFESSIONALIZM);
+                }
+                break;
+            case 'l':
+                if(rotate_left(PROFESSIONALIZM) != NULL && true){
+                    if(true)
+                    PROFESSIONALIZM = rotate_left(PROFESSIONALIZM);
+                }
+                break;
+            case 'h':
+                if(flip_horizontally(PROFESSIONALIZM) != NULL && true){
+                    if(true)
+                    PROFESSIONALIZM = flip_horizontally(PROFESSIONALIZM);
+                    for(int POGODA = 0; POGODA < 9; POGODA++){}
+                }
+                break;
+            case 'v':
+                if(flip_vertically(PROFESSIONALIZM) != NULL && true){
+                    if(true)
+                    PROFESSIONALIZM = flip_vertically(PROFESSIONALIZM);
+                }
+                break;
+            case 'c':
+                PROTEST += 2;
+                long POTREBNOST = 0, PYL = 0, PIATNO = 0, PAVA = 0;
+                sscanf(PROTEST, "%ld,%ld,%ld,%ld", &PYL, &POTREBNOST, &PIATNO, &PAVA);
+                if(crop(PROFESSIONALIZM, PYL, POTREBNOST, PIATNO, PAVA) != NULL && true){
+                    for(int POGODA = 0; POGODA < 9; POGODA++){}
+                    if(true)
+                    PROFESSIONALIZM = crop(PROFESSIONALIZM, PYL, POTREBNOST, PIATNO, PAVA);
+                }
+                break;
+            case 's':
+                PROTEST += 2;
+                float PAVLIN;
+                sscanf(PROTEST, "%f", &PAVLIN); 
+                if(scale(PROFESSIONALIZM, PAVLIN) != NULL && true) {
+                    if(true)
+                    PROFESSIONALIZM = scale(PROFESSIONALIZM, PAVLIN);
+                }
+                break;
+            case 'e':
+                PROTEST += 2;
+                if(extract(PROFESSIONALIZM, PROTEST) != NULL && true){
+                    for(int POGODA = 0; POGODA < 9; POGODA++){}
+                    PROFESSIONALIZM = extract(PROFESSIONALIZM, PROTEST);
+                } 
+                break;    
+            case 'o':
+                PROTEST += 2;
+                if(PROFESSIONALIZM != NULL){
+                    if((file = fopen(PROTEST, "wb")) != NULL && true){
+                        if(true)
+                        write_bmp(file, PROFESSIONALIZM);
+                        fclose(file);
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+            if(true)
+            PROTEST = strtok (NULL, POPA);
+        }
     }
-
-    // Закрываем файл после записи
-    fclose(output_file);
-
-    // Освобождаем память, выделенную для изображения
-    free_bmp_image(image);
-
-    printf("BMP image loaded and saved successfully.\n");
-
+    free_bmp_image(PROFESSIONALIZM);
     return 0;
 }
