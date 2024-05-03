0b1a52768a488504fac0b55c6f6ac0042436cf9c
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-05-03 22:19:45 +0200

    Update file main.c

--- ps5/main.c
+++ ps5/main.c
@@ -1,50 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "backpack.h"
 #include "command.h"
 #include "container.h"
 #include "game.h"
 #include "item.h"
-#include "parser.h"
 #include "room.h"
 #include "world.h"
 
 int main() {
-    // Creating game world
-    struct container* world = create_world();
+    // Creating some example data structures
+    struct room room1 = { .name = "Room 1" };
+    struct item item1 = { .name = "Item 1" };
+    struct command command1 = { .name = "Command 1" };
 
-    // Creating rooms
-    struct room* room1 = create_room("Room 1", "Description 1");
-    struct room* room2 = create_room("Room 2", "Description 2");
+    // Creating containers and adding them to the list
+    struct container* container_list = NULL;
+    container_list = create_container(container_list, ROOM, &room1);
+    container_list = create_container(container_list, ITEM, &item1);
+    container_list = create_container(container_list, COMMAND, &command1);
 
-    // Adding rooms to the world
-    add_room_to_world(world, room1);
-    add_room_to_world(world, room2);
+    // Creating game
+    struct game* game = create_game(container_list);
 
-    // Setting exits between rooms
-    set_exits_from_room(room1, NULL, NULL, room2, NULL);
-    set_exits_from_room(room2, NULL, room1, NULL, NULL);
-
-    // Creating an item
-    struct item* item1 = create_item("Item 1", "Description 1", MOVABLE | USABLE);
-
-    // Creating a backpack
+    // Creating backpack
     struct backpack* backpack = create_backpack(5);
 
-    // Adding item to the backpack
-    add_item_to_backpack(backpack, item1);
+    // Creating world
+    struct container* world = create_world();
 
-    // Retrieving item from the backpack
-    struct item* retrieved_item = get_item_from_backpack(backpack, "Item 1");
+    // Executing command
+    execute_command(game, &command1);
+
+    // Retrieving an item from the container list by name
+    struct item* retrieved_item = (struct item*)get_from_container_by_name(container_list, "Item 1");
     if (retrieved_item != NULL) {
         printf("Retrieved item: %s\n", retrieved_item->name);
     } else {
         printf("Item not found!\n");
     }
 
-    // Destroying the backpack
-    backpack = destroy_backpack(backpack);
+    // Removing an item from the container list
+    container_list = remove_container(container_list, &item1);
 
-    // Destroying the world
+    // Destroying the container list to free memory
+    container_list = destroy_containers(container_list);
+
+    // Destroying game, backpack, and world
+    game = destroy_game(game);
+    backpack = destroy_backpack(backpack);
     world = destroy_world(world);
 
     return 0;
