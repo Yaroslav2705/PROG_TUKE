5690eb06dab86788106ef5d4eb1e47bcb8a7b8b8
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-05-03 22:22:26 +0200

    Update file main.c

--- ps5/main.c
+++ ps5/main.c
@@ -9,18 +9,8 @@
 #include "world.h"
 
 int main() {
-    // Creating some example data structures
-    struct room room1 = { .name = "Room 1" };
-    struct item item1 = { .name = "Item 1" };
-    struct command command1 = { .name = "Command 1" };
-
-    // Creating containers and adding them to the list
-    struct container* container_list = NULL;
-    container_list = create_container(container_list, ROOM, &room1);
-    container_list = create_container(container_list, ITEM, &item1);
-    container_list = create_container(container_list, COMMAND, &command1);
-
     // Creating game
+    struct container* container_list = NULL;
     struct game* game = create_game(container_list);
 
     // Creating backpack
@@ -29,22 +19,26 @@ int main() {
     // Creating world
     struct container* world = create_world();
 
-    // Executing command
-    execute_command(game, &command1);
+    // Creating room
+    struct room* room1 = create_room("Room 1", "This is room 1");
+    world = add_room_to_world(world, room1);
 
-    // Retrieving an item from the container list by name
-    struct item* retrieved_item = (struct item*)get_from_container_by_name(container_list, "Item 1");
+    // Creating item
+    struct item* item1 = create_item("Item 1", "This is item 1", MOVABLE | USABLE);
+
+    // Adding item to backpack
+    add_item_to_backpack(backpack, item1);
+
+    // Retrieving item from backpack
+    struct item* retrieved_item = get_item_from_backpack(backpack, "Item 1");
     if (retrieved_item != NULL) {
         printf("Retrieved item: %s\n", retrieved_item->name);
     } else {
         printf("Item not found!\n");
     }
 
-    // Removing an item from the container list
-    container_list = remove_container(container_list, &item1);
-
-    // Destroying the container list to free memory
-    container_list = destroy_containers(container_list);
+    // Removing item from backpack
+    delete_item_from_backpack(backpack, item1);
 
     // Destroying game, backpack, and world
     game = destroy_game(game);
