13661896fa7e80ae4323565ce4a363bfab6134fd
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-05-03 23:03:33 +0200

    Update file command.c

--- ps5/command.c
+++ ps5/command.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include "command.h"
 
+char* strdup(const char* str) {
+    if (str == NULL) return NULL;
+
+    size_t len = strlen(str);
+    char* new_str = (char*)malloc(len + 1);
+    if (new_str == NULL) return NULL;
+
+    strcpy(new_str, str);
+    return new_str;
+}
+
+
 struct command* create_command(char* name, char* description, char* pattern, size_t nmatch) {
     if (name == NULL || description == NULL) {
         return NULL; // Command cannot be created without name or description
