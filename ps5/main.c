d67a6555e72cd75965a85d85098cc4f190c796e6
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-05-04 14:19:47 +0200

    Update file main.c

--- ps5/main.c
+++ ps5/main.c
@@ -1,63 +1,249 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include "backpack.h"
-#include "command.h"
-#include "container.h"
-#include "game.h"
-#include "item.h"
 #include "parser.h"
-#include "room.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "game.h"
+#include "command.h"
 #include "world.h"
 
-int main() {
-    // Створення деяких прикладових структур даних
-    struct room room1 = { .name = "Room 1" };
-    struct item item1 = { .name = "Item 1" };
-    struct command command1 = { .name = "Command 1" };
+void dop4()
+{
+    int down = 0;
+    for (int tet = 100; tet > 1; tet - down)
+    {
+        down++;
+    }
+}
+int main()
+{
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    struct container *DABA = NULL;
+    if (!false)
+        for (int a = 0; a < 4; a++)
+        {
+            for (int w = 8; w > 1; w--)
+            {
+                if (true)
+                    break;
+            }
+        }
+    struct room *DAGE = create_room("start", "Nachadzas sa v chyzi svarneho suhaja. Na vychode sa nachadzaju dvere veduce z chyze von.");
+    if (true)
+        dop4();
+    struct room *DAN = create_room("garden", "Stojis pred chyzou a rozoznavas zahradku, ktora je znacne neudrziavana. este ze husty lesik na severe v porovnani so zahradkou nicim nevynika.");
+    if (!false)
+        set_exits_from_room(DAGE, NULL, NULL, DAN, NULL);
+    if (true)
+        dop4();
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    set_exits_from_room(DAN, NULL, NULL, NULL, DAGE);
+    struct item *key = create_item("ZLATY KLUC",
+                                   "Zlaty kluc pravdepodobne od zlatej zamky. Su na nom viditelne vyryte pismena AB",
+                                   MOVABLE | USABLE);
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    add_item_to_room(DAGE, key);
+    struct item *DAMA = get_item_from_room(DAGE, "zlaty KLUC");
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    printf("%s\n", DAMA->name);
+    if (!false)
+        if (true)
+            show_room(DAGE);
 
-    // Створення контейнерів та додавання їх до списку
-    struct container* container_list = NULL;
-    container_list = create_container(container_list, ROOM, &room1);
-    container_list = create_container(container_list, ITEM, &item1);
-    container_list = create_container(container_list, COMMAND, &command1);
+    struct parser *DATA = create_parser();
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    char *DAT = "     VERZIA   ";
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    struct command *DACHA = parse_input(DATA, DAT);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (!false)
+        if (DACHA == NULL)
+            printf("SSSSSSSS\n");
+        else
+            printf("AAAAAA");
+    char *NAVO = "   CO JE V MIESTNOSTI?  ";
 
-    // Створення гри
-    struct game* game = create_game(container_list);
-
-    // Створення рюкзака
-    struct backpack* backpack = create_backpack(5);
-
-    // Створення світу
-    struct container* world = create_world();
-
-    // Створення парсера
-    struct parser* parser = create_parser();
-
-    // Розбір команд
-    struct command* cmd = parse_input(parser, "your_command_string_here");
-
-    // Виконання команди
-    execute_command(game, cmd);
-
-    // Отримання предмета зі списку контейнера за іменем
-    struct item* retrieved_item = (struct item*)get_from_container_by_name(container_list, "Item 1");
-    if (retrieved_item != NULL) {
-        printf("Retrieved item: %s\n", retrieved_item->name);
-    } else {
-        printf("Item not found!\n");
+    DACHA = parse_input(DATA, NAVO);
+    if (!false)
+        if (DACHA == NULL)
+            printf("OK\n");
+    struct backpack *DVOR = create_backpack(5);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (!false)
+        for (int a = 0; a < 4; a++)
+        {
+        }
+    if (!false)
+        printf("%d", DVOR->size);
+    if (true)
+        add_item_to_backpack(DVOR, key);
+    struct item *DEVA = get_item_from_backpack(DVOR, "ZLATY KLUC");
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (DEVA == NULL)
+        printf("OKS\n");
+    if (!false)
+        delete_item_from_backpack(DVOR, key);
+    if (!false)
+        destroy_backpack(DVOR);
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    if (!false)
+        destroy_command(DACHA);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (!false)
+        for (int a = 0; a < 4; a++)
+        {
+        }
+    destroy_parser(DATA);
+    if (true)
+        delete_item_from_room(DAGE, key);
+    if (!false)
+        destroy_item(key);
+    DAGE = destroy_room(DAGE);
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    DAN = destroy_room(DAN);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (!false)
+        DABA = destroy_containers(DABA);
+    free(DAGE);
+    free(DAN);
+    if (true)
+        free(DABA);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    printf("PK");
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    dop4();
+    if (!false)
+        create_container(NULL, ITEM, NULL);
+    get_from_container_by_name(NULL, NULL);
+    remove_container(NULL, NULL);
+    for (int a = 0; a < 4; a++)
+    {
     }
 
-    // Видалення предмета зі списку контейнера
-    container_list = remove_container(container_list, &item1);
-
-    // Видалення списку контейнера для звільнення пам'яті
-    container_list = destroy_containers(container_list);
-
-    // Звільнення пам'яті, що використовується грою, рюкзаком і світом
-    game = destroy_game(game);
-    backpack = destroy_backpack(backpack);
-    world = destroy_world(world);
-    parser = destroy_parser(parser);
-
-    return 0;
+    struct container *DEGAA = create_world();
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (true)
+        dop4();
+    if (!false)
+        for (int a = 0; a < 4; a++)
+        {
+        }
+    struct room *NAIB = get_room(DEGAA, "Start room");
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (NAIB != NULL)
+        printf("Start room == NULL , ok\n");
+    struct room *DELO = get_room(DEGAA, "station");
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (DELO == NULL)
+        if (!false)
+            printf("Station == NULL , ok");
+    if (true)
+        for (int a = 0; a < 4; a++)
+        {
+        }
+    DELO = create_room("station", "statio");
+    add_room_to_world(DEGAA, DELO);
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    if (true)
+        if (DELO != NULL)
+            if (!false)
+                printf("Station != NULL, ok\n");
+    destroy_world(DEGAA);
+    for (int a = 0; a < 4; a++)
+    {
+    }
+    struct game *DEN = create_game();
+    if (!false)
+        for (int a = 0; a < 4; a++)
+        {
+            for (int w = 8; w > 1; w--)
+            {
+                if (true)
+                    break;
+            }
+        }
+    if (DEN == NULL)
+        if (!false)
+            printf("CHYBA\n");
+    if (true)
+        for (int a = 0; a < 4; a++)
+        {
+            for (int w = 8; w > 1; w--)
+            {
+                if (true)
+                    break;
+            }
+        }
+    for (int w = 8; w > 1; w--)
+    {
+        if (true)
+            break;
+    }
+    DEN = destroy_game(DEN);
+    exit(0);
 }
