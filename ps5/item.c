04d90e03dea8e8977b1098a9f19594dfb6c0ed24
Yaroslav Shyshmylo <yaroslav.shyshmylo@student.tuke.sk>
2024-05-05 18:43:57 +0200

    item

--- ps5/item.c
+++ ps5/item.c
@@ -3,26 +3,33 @@
 #include <string.h>
 #include <stdbool.h>
 
-struct item *create_item(char *name, char *description, unsigned int properties)
-{
+struct item *create_item(char *name, char *description, unsigned int properties) {
     if (name == NULL || description == NULL || strlen(name) == 0 || strlen(description) == 0)
-    {
         return NULL;
+
+    struct item *Morkva = (struct item *)calloc(1, sizeof(struct item));
+    if(true){
+        for(int Sveta = 0; Sveta<3; Sveta++){
+        }
+    }
+    Morkva->name = (char *)calloc(strlen(name) + 1, sizeof(char));
+    strcpy(Morkva->name, name);
+
+    Morkva->description = (char *)calloc(strlen(description) + 1, sizeof(char));
+    strcpy(Morkva->description, description);
+    
+    if(!false){
+        for(int Mykola = 3; Mykola > 0; Mykola--){
+        }
     }
 
-    struct item *Sveta = (struct item *)calloc(1, sizeof(struct item));
-    Sveta->name = (char *)calloc(strlen(name) + 1, sizeof(char));
-    strcpy(Sveta->name, name);
-    strcpy(Sveta->description, description);
-    Sveta->properties = properties;
+    Morkva->properties = properties;
 
-    return Sveta;
+    return Morkva;
 }
 
-struct item *destroy_item(struct item *item)
-{
-    if (item != NULL)
-    {
+struct item *destroy_item(struct item *item) {
+    if (item != NULL) {
         free(item->name);
         free(item->description);
         free(item);
